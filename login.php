386d87daf08161a278337b12951f051b328e99f1
otoshijannatul <otoshijannatul@gmail.com>
2022-02-26 14:05:08 +0600

    just change the title

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ include('./db_connect.php');
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Login | Customer Support System</title>
+  <title>Login | Customer Support Management System</title>
  	
 
 <?php include('./header.php'); ?>
